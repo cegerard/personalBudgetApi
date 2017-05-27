f23f308ab97448cf64afd52a8ca2916e9bc18a05
cegerard <git@eriatand.org>
2017-05-27 19:39:29 +0200

    filter usless methods

--- common/models/budget-line.js
+++ common/models/budget-line.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Budgetline) {
+  Budgetline.disableRemoteMethodByName('findOne');
+  Budgetline.disableRemoteMethodByName('count');
+  Budgetline.disableRemoteMethodByName('exists');
+  Budgetline.disableRemoteMethodByName('upsert');
+  Budgetline.disableRemoteMethodByName('updateAll');
+  Budgetline.disableRemoteMethodByName('replaceOrCreate');
+  Budgetline.disableRemoteMethodByName('createChangeStream');
+  Budgetline.disableRemoteMethodByName('upsertWithWhere');
+  Budgetline.disableRemoteMethodByName('prototype.updateAttributes');
 
+  Budgetline.sharedClass.findMethodByName('replaceById', true).http = [{ verb: 'put', path: '/:id' }];
 };
--- common/models/expense.js
+++ common/models/expense.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(Expense) {
+  Expense.disableRemoteMethodByName('findOne');
+  Expense.disableRemoteMethodByName('count');
+  Expense.disableRemoteMethodByName('exists');
+  Expense.disableRemoteMethodByName('upsert');
+  Expense.disableRemoteMethodByName('updateAll');
+  Expense.disableRemoteMethodByName('replaceOrCreate');
+  Expense.disableRemoteMethodByName('createChangeStream');
+  Expense.disableRemoteMethodByName('upsertWithWhere');
+  Expense.disableRemoteMethodByName('prototype.updateAttributes');
+  Expense.disableRemoteMethodByName('deleteById');
+
+  Expense.sharedClass.findMethodByName('replaceById', true).http = [{ verb: 'put', path: '/:id' }];
 
 };
