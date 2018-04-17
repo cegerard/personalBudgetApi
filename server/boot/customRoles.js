ba3ce47ea3b9765d30b7bf3b8b9b42476d2a930a
Cédric GERARD <git@eriatand.org>
2018-04-17 22:29:33 +0200

    feat(admin): add default system administrator

--- server/boot/customRoles.js
+++ server/boot/customRoles.js
@@ -1,22 +1,50 @@
 'use strict'
 
+const { get } = require('lodash');
+
 module.exports = (app) => {
     const Role = app.models.Role;
+    const endUser = app.models.endUser;
+    const RoleMapping = app.models.RoleMapping;
 
     console.log('Initiate application roles.');
     
     //create the admin role
     Role.find({name: 'admin'})
-        .then((role) => {
-            console.log(`Keep existing ${role[0].name} role.`);
+        .then((adminRole) => {
+            console.log(`Keep existing ${adminRole[0].name} role.`);
+            return adminRole[0];
         })
         .catch(() => {
-            Role.create({name: 'admin'})
-            .then((role) => {
-                console.log(`Role ${role.name} created`);
+            return Role.create({name: 'admin'})
+            .then((newAdminRole) => {
+                console.log(`Role ${newAdminRole.name} created`);
+                return newAdminRole;
             })
             .catch((err) => {
                 throw err;
             });
+        })
+        .then((adminRole) => {
+            endUser.create({
+                firstName: "administrator",
+                lastName: "administrator",
+                email: "admin@personalbudget.com",
+                password: "adminadmin"
+            })
+            .then((admin) => {
+                console.log('Default admin user created');
+                adminRole.principals.create({
+                    principalType: RoleMapping.USER,
+                    principalId: admin.id
+                })
+            })
+            .catch((err) => {
+               if(get(err, 'details.messages.email[0]', '') == 'Email already exists') {
+                    console.log('Use existing admin user');
+                } else {
+                    throw err;
+                }
+            })
         });
 };
