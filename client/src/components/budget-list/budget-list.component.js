a1a788679732bd6f15f83423d7cd16fd2baa2f73
cegerard <git@eriatand.org>
2018-12-31 13:49:56 +0100

    use multifiles componenents

---
+++ client/src/components/budget-list/budget-list.component.js
@@ -0,0 +1,7 @@
+module.exports = class {
+    onCreate() {
+        // TODO call service to list budget lines
+        // Feed the state with budget list to display
+        this.state = {};
+    }
+}
