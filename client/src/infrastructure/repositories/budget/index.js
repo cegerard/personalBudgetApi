d39affd5da27e91bc9cba54fdd68f04870c21ec9
cegerard <git@eriatand.org>
2019-07-16 23:06:24 +0200

    feat: use env var to setup repositories

--- client/src/infrastructure/repositories/budget/index.js
+++ client/src/infrastructure/repositories/budget/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const inMemoryRepository = require('./inMemory');
+let budgetRepository;
+if ('production' === process.env.NODE_ENV) { // production
+    // TODO add loopback repository
+    console.warn('\x1b[33mWARN: Production mode use in memory budget repository !\x1b[39m');
+    budgetRepository = require('./inMemory');
+} else { // development
+    budgetRepository = require('./inMemory');
+}
 
-module.exports = inMemoryRepository;
+module.exports = budgetRepository;
