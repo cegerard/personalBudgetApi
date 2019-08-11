bc7bb77c477e2a22e837e669c94d8e3c4e276e0b
Cedric Gerard <git@eriatand.org>
2019-08-11 17:42:24 +0200

    add bduget line create form

--- client/src/components/budget-form/budget-form.component.js
+++ client/src/components/budget-form/budget-form.component.js
@@ -1,3 +1,40 @@
 'use strict';
 
-module.exports = class {}
+module.exports = class {
+  onCreate() {
+    // Init component state
+    this.state = {
+      createForm: {
+        name: '',
+        amount: 0,
+        description: '',
+        period: 'monthly',
+        date: new Date()
+      }
+    };
+  }
+
+  setName(event) {
+    this.state.createForm.name = event.target.value;
+  }
+
+  setAmount(event) {
+    this.state.createForm.amount = event.target.value;
+  }
+
+  setDescription(event){
+    this.state.createForm.description = event.target.value;
+  }
+
+  setPeriod(event) {
+    this.state.createForm.period = event.target.value;
+  }
+
+  setDate(event) {
+    this.state.createForm.date = new Date(event.target.value);
+  }
+
+  createBudget() {
+    console.log(this.state.createForm);
+  }
+}
