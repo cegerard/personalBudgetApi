88b6704159ceb2770815d8d1b6cfa962b8daf29f
Cedric Gerard <git@eriatand.org>
2019-12-27 12:49:16 +0100

    fix: nested routes definitions

--- server/boot/initMethods.js
+++ server/boot/initMethods.js
@@ -3,5 +3,10 @@
 const {models} = require('../methods');
 
 module.exports = (app) => {
+  // Init nested queries
+  const endUser = app.models.endUser;
+  endUser.nestRemoting('budgetLines');
+
+  // Init custom methods
   models.buildBudgetLineMethods(app);
 };
