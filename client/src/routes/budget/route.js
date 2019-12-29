4bee2c5ccbe322ddc65a9303067bea186b57bb55
Cedric Gerard <git@eriatand.org>
2019-12-30 00:41:20 +0100

    feat: add available budget

--- client/src/routes/budget/route.js
+++ client/src/routes/budget/route.js
@@ -8,12 +8,18 @@ const template = require('./index.marko');
 
 exports.paths = '/budgets/:id';
 
-exports.handler = async(input, out) => {
+exports.handler = async (input, out) => {
   const context = getUserSession(input.headers);
   const budget = await budgetRepository.getById(input.params.id, context);
   const budgetExpenses = await expenseRepository.search({
     budgetLine: input.params.id,
   }, context);
+
+  const totalExpense = budgetExpenses.reduce((total, expense) => {
+    return total + expense.amount;
+  }, 0);
+
+  budget.available = budget.budget - totalExpense;
 
   template.render({
     budget,
